e8caa9ef9d6dfc41e33319fd2dd4139f953d88e0
Rish7223 <rishabh.7223@gmail.com>
2020-09-04 11:52:58 +0530

    well formatted code

--- web-link-crawler.py
+++ web-link-crawler.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
 import re
-
+       
 # bypassing ssl cerificates errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -17,11 +17,15 @@ html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
 tags = soup('a')
+print("==================================");
+print("Here are the List of LINKS: \n")
+
 for tag in tags:
    links = re.findall('href="([A-Za-z0-9].*)">', str(tag))
    if len(links) > 0:
      data = links[0].split('"')[0]
      if data.startswith("http"):
       print(data)
+print("==================================");
    
 
